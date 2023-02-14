73a83afab22be2af14be071646ea202110d83ae4
wjohansson <william.johansson1999@outlook.com>
2023-02-14 12:25:47 +0100

    Add project files.

---
+++ FormsLibrary/Models/FormModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace FormsLibrary.Models
+{
+    public class FormModel
+    {
+        public string FormTitle { get; set; } = "";
+        public string FormDescription { get; set; } = "";
+        public bool IsFocused { get; set; }
+        public ICollection<QuestionModel> Components { get; set; }
+
+        public FormModel()
+        {
+            Components = new List<QuestionModel>();
+        }
+    }
+}
---
+++ FormsLibrary/Models/QuestionModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormsLibrary.Models
+{
+    public class QuestionModel
+    {
+        public string QuestionTitle { get; set; } = "";
+        public QuestionOptions Option { get; set; }
+    }
+
+    public enum QuestionOptions
+    {
+        Flerval,
+        Enkelval,
+        Rullgardin,
+        Skala,
+        Datum,
+        Tid,
+        Kort,
+        Lång
+    }
+}
